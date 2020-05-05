b3d57d23e54564fc6e70e61319dd1cb07131c6db
louyilin <louyilin1996@gmail.com>
2020-05-05 19:28:09 -0400

    Create bayes.py

---
+++ Bayes/bayes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project -> File   ：Spam-Filtering -> bayes
+@IDE    ：PyCharm
+@Author ：Yilin Lou
+@Date   ：5/5/20 7:27 下午
+@Group  ：Stevens Institute of technology
+'''
