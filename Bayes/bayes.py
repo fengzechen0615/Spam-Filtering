493ed48e6deb7d5bfb7448504a9fbfab62233cd8
louyilin <louyilin1996@gmail.com>
2020-05-08 01:11:18 -0400

    finish

--- Bayes/bayes.py
+++ Bayes/bayes.py
@@ -11,7 +11,7 @@
 import time
 import nltk
 import ssl
-
+import time
 # try:
 #     _create_unverified_https_context = ssl._create_unverified_context
 # except AttributeError:
@@ -33,7 +33,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.decomposition import TruncatedSVD  # 降维
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB, GaussianNB  # 伯努利分布的贝叶斯公式
 from sklearn.metrics import f1_score, precision_score, recall_score
-
+#############################################################################   start
 # 1、文件数据读取
 from Bayes.split_train import split_train
 
@@ -51,8 +51,21 @@ df.columns = ['label', 'text', 'class']
 df.drop('label', axis=1, inplace=True)
 # print(df.shape)
 
+# for picture
+# accuracy list
+acc=[]
+
+# time list
+times=[]
+# PCV
+components=np.arange(10,510,10)
+a=np.arange(1,11,1)
+
+#components=[10,20,30,40,50,60,70,80,90,100]
+
 #
-for m in range(20):
+for m in range(10):
+    start = time.time()
     # 2、train and test
     # x_train, x_test, y_train, y_test = train_test_split(df, df["class"], test_size=0.2, random_state=0)
     train, test = split_train(df, 0.5)
@@ -91,7 +104,7 @@ for m in range(20):
     # print(stop)
     transformer = TfidfVectorizer(norm="l2", use_idf=True,
                                   stop_words=STOPWORDS)  # TF-IDF(term frequency-inverse document frequency)词频-逆向文件频率
-    svd = TruncatedSVD(n_components=30)  # 奇异值分解，降维
+    svd = TruncatedSVD(n_components=200)  # 奇异值分解，降维
     # print(svd)
     train_cut_text = list(x_train.astype("str"))
     # print(train_cut_text)
@@ -114,7 +127,7 @@ for m in range(20):
     nb1 = BernoulliNB(alpha=1.0, binarize=0.001)  # Bayes
     nb2 = GaussianNB()  # GaussianNB
 
-    model = nb1.fit(data, y_train)
+    model = nb2.fit(data, y_train)
 
     # print(nb1.score(data, y_train))
 
@@ -143,10 +156,42 @@ for m in range(20):
             fail = fail + 1
     k=m+1
     print("The %d time"%k)
+    accuracy=count / len(y_test)
 
-    print("Accuracy rate: ", count / len(y_test))
+    print("Accuracy rate: ", accuracy)
 
     print("The times uncorrect are : ", fail)
 
-    print('\n')
+    end = (time.time() - start)
+    print("Time used:", end)
 
+    print('\n')
+    acc.append(accuracy)
+    times.append(end)
+
+
+print("The average accyracy is : ",np.mean(acc))
+
+# plot函数作图
+# plt.plot(components,acc)
+# plt.savefig("components_acc.png")
+#
+# # show函数展示出这个图，如果没有这行代码，则程序完成绘图，但看不到
+# plt.show()
+#
+# plt.plot(components,times)
+# plt.savefig("components_times.png")
+#
+# plt.show()
+
+plt.plot(a,acc)
+plt.title('GaussianNB--Accuracy: 10 times')
+plt.savefig("GaussianNB_acc.png")
+plt.show()
+
+# plt.plot(a,acc)
+# plt.title('BernoulliNB--Accuracy: 10 times')
+# plt.savefig("BernoulliNB_acc.png")
+# plt.show()
+
+########################################################################   end
